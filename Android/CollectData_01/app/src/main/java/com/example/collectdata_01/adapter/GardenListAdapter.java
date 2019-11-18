b4e470475889ed26e442cd8f9ba0b3ed1db0bbee
saber110 <15675123342@163.com>
2019-11-18 18:30:37 +0800

    update map link, neighbourWorking

--- Android/CollectData_01/app/src/main/java/com/example/collectdata_01/adapter/GardenListAdapter.java
+++ Android/CollectData_01/app/src/main/java/com/example/collectdata_01/adapter/GardenListAdapter.java
@@ -1,7 +1,6 @@
 package com.example.collectdata_01.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.collectdata_01.R;
-import com.example.map.baidu_map.BaiduMapActivity;
 import com.example.map.dao.SearchGardenResultDao;
 
 public class GardenListAdapter extends RecyclerView.Adapter<GardenListAdapter.ViewHolder> {
@@ -19,10 +17,12 @@ public class GardenListAdapter extends RecyclerView.Adapter<GardenListAdapter.Vi
     private Context context;
     private LayoutInflater inflater;
     private SearchGardenResultDao.DataBean result;
+    private TextView neighbourWorking;
 
-    public GardenListAdapter(Context context, SearchGardenResultDao.DataBean result) {
+    public GardenListAdapter(Context context, SearchGardenResultDao.DataBean result, TextView neighbourWorking) {
         this.context = context;
         this.result = result;
+        this.neighbourWorking = neighbourWorking;
         inflater = LayoutInflater.from(context);
     }
 
@@ -33,19 +33,18 @@ public class GardenListAdapter extends RecyclerView.Adapter<GardenListAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
-        String name = result.getBuildingKinds().get(position).getKindName()+"";
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        final String name = result.getBuildingKinds().get(position).getKindName()+"";
         final String id = result.getBuildingKinds().get(position).getId();
         holder.textView.setText(name);
+
         /**
          * 点击小区后进行跳转
          */
         holder.searchResultView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, BaiduMapActivity.class);
-                intent.putExtra("gardenId",id);
-                context.startActivity(intent);
+                neighbourWorking.setText(name);
             }
         });
     }
--- Android/CollectData_01/app/src/main/java/com/example/login/login.java
+++ Android/CollectData_01/app/src/main/java/com/example/login/login.java
@@ -1,20 +1,27 @@
 package com.example.login;
 
+import android.Manifest;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.transition.Explode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.collectdata_01.MainActivity;
 import com.example.collectdata_01.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.iemi.getIemi;
 
 public class login extends AppCompatActivity {
 
@@ -22,22 +29,25 @@ public class login extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btGo;
-    private CardView cv;
     private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_activity_one);
+        checkPermissions(needPermissions);
         initView();
+
         setListener();
     }
 
     private void initView() {
         etUsername = findViewById(R.id.et_username);
+        etUsername.setText(getIemi.getIMEI(this));
+        etUsername.setFocusable(false);
+        etUsername.setFocusableInTouchMode(false);
         etPassword = findViewById(R.id.et_password);
         btGo = findViewById(R.id.bt_go);
-        cv = findViewById(R.id.cv);
         fab = findViewById(R.id.fab);
     }
 
@@ -76,5 +86,42 @@ public class login extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         fab.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 需要进行检测的权限数组
+     */
+    protected String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CAMERA,
+    };
+
+
+    /**
+     * 检测权限
+     * 地图sdk所必需的权限
+     *
+     * @param needPermissions
+     */
+    private void checkPermissions(String[] needPermissions) {
+        // 假如手机的版本大于23 ， 则申请权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : needPermissions) {
+                // 假如已经授权
+                if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+                    continue;
+                } else {
+                    ActivityCompat.requestPermissions(this, needPermissions, 1);
+                }
+            }
+        }
+        // 假如手机版本低于23
+        else {
+            Log.d("授权tag", "checkPermissions: 手机版本低于23");
+        }
     }
 }
