717a1f50b84b7d2f16610ae7a47e8df19fbbff52
possibleit <33998680+possibleit@users.noreply.github.com>
2019-12-04 10:48:58 +0800

    楼栋调查Dialog

--- Android/app/src/main/java/com/example/collectdata/DataActivity.java
+++ Android/app/src/main/java/com/example/collectdata/DataActivity.java
@@ -1,13 +1,19 @@
 package com.example.collectdata;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.collectdata.listener.DataListener;
 import com.example.collectdata.tools.ConstTools;
 import com.example.collectdata_01.R;
+import com.example.dialog.CreatDialog;
 
 /**
  * 数据采集页面
@@ -18,6 +24,13 @@ public class DataActivity extends AppCompatActivity {
     private LinearLayout louzhuangdiaocha;
     private LinearLayout xiaoquxinxi;
     private LinearLayout louzhuanxinxi;
+
+    private Dialog buildingNumDialog;
+    private View BuildNumView;
+    private Button buttonSubmit;
+    private Button buttonCancel;
+    private EditText editBuildingNum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +43,44 @@ public class DataActivity extends AppCompatActivity {
         louzhuangdiaocha = this.findViewById(R.id.data_louzhuangdiaocha);
         louzhuanxinxi = this.findViewById(R.id.data_louzhuangxinxi);
 
+        initDialog();
         //添加监听器
         xiaoqugaikuang.setOnClickListener(new DataListener(this, ConstTools.XIAOQUGAIKUANG, DataActivity.this));
         xiaoquxinxi.setOnClickListener(new DataListener(this, ConstTools.XIAOQUXINXI, DataActivity.this));
         louzhuanxinxi.setOnClickListener(new DataListener(this, ConstTools.LOUZHUANGXINXI, DataActivity.this));
-        louzhuangdiaocha.setOnClickListener(new DataListener(this, ConstTools.LOUZHUANGDIAOCHA, DataActivity.this));
-
+//        louzhuangdiaocha.setOnClickListener(new DataListener(this, ConstTools.LOUZHUANGDIAOCHA, DataActivity.this));
+        louzhuangdiaocha.setOnClickListener(louzhuangdiaochaListener);
     }
+
+    private void initDialog(){
+        BuildNumView = getLayoutInflater().inflate(R.layout.building_num_post, null);
+        buildingNumDialog = CreatDialog.createChangeMarkDialog(DataActivity.this, BuildNumView);
+        buttonSubmit = BuildNumView.findViewById(R.id.building_num_submit);
+        buttonCancel = BuildNumView.findViewById(R.id.building_num_cancel);
+        editBuildingNum = BuildNumView.findViewById(R.id.building_num_edit);
+    }
+     View.OnClickListener louzhuangdiaochaListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            buildingNumDialog.show();
+            buttonSubmit.setOnClickListener(subminListener);
+            buttonCancel.setOnClickListener(cancelListener);
+        }
+    };
+
+    View.OnClickListener subminListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(DataActivity.this,"提交测试成功",Toast.LENGTH_SHORT).show();
+
+            buildingNumDialog.dismiss();
+        }
+    };
+     View.OnClickListener cancelListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(DataActivity.this,"取消测试成功",Toast.LENGTH_SHORT).show();
+            buildingNumDialog.dismiss();
+        }
+    };
 }
