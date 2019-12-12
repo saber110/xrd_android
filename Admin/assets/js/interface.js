62e8da729425b099bd180a1dc3d8526769e25185
saber110 <15675123342@163.com>
2019-12-12 19:15:27 +0800

    code format

--- Admin/assets/js/interface.js
+++ Admin/assets/js/interface.js
@@ -389,20 +389,3 @@ function downloadExcel(url, gardenId) {
 function downloadPoicture(gardenId) {
     downloadExcel(pictureExport, gardenId);
 }
-
-$.fn.serializeObject = function()    
-{    
-   var o = {};    
-   var a = this.serializeArray();    
-   $.each(a, function() {    
-       if (o[this.name]) {    
-           if (!o[this.name].push) {    
-               o[this.name] = [o[this.name]];    
-           }    
-           o[this.name].push(this.value || '');    
-       } else {    
-           o[this.name] = this.value || '';    
-       }    
-   });    
-   return o;    
-};
